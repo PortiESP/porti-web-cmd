814dcf362e2b28700dfd2af4da403f850081a585
porti <portiesp@gmail.com>
2023-08-26 18:47:17 +0200

    Updated introduction and logo

--- src/assets/introduction.jsx
+++ src/assets/introduction.jsx
@@ -1,28 +1,34 @@
 const introduction = [
   `
 
-  INTRODUCTION
-  ------------
+  ┌──────────────┐
+  │ INTRODUCTION │
+  └──────────────┘
 
-  Greetings user!, Let me introduce myself in a quite fast...
+      Greetings user!, Let me introduce myself in a quite fast...
 
-    - My name is Jaime Portillo, but everyone knows me as Porti ;)
-    - I spend my time learning and developing software
-    - I am not only into Web Development but also in any other topic related to software: Cibersecurity, Low-level Programming, OS Managemenget, Cloud & DBs, ...
-
-    
-  WHAT IS THIS?
-  -------------
-
-  This project is a terminal-based profolio, the user will use some predefined commands to help him getting to know me.
-
-  The terminal also provide a few keyboard shotcuts and features from the native terminal emulators.
+        - My name is Jaime Portillo, but everyone knows me as Porti ;)
+        - I spend my time learning and developing software
+        - I am not only into Web Development but also in any other topic related to software: Cibersecurity, Low-level Programming, OS Managemenget, Cloud & DBs, ...
 
 
-  HOW TO START!
-  -------------
+  ┌───────────────┐
+  │ WHAT IS THIS? │
+  └───────────────┘
+      
+      - This project is a terminal-based profolio, the user will use some predefined commands to help him getting to know me.
+      - The terminal also implements some features such as: keybinds, command autocompletion or commands history`,
+  <>
+    {"      - "}Take a look to the project code at the{" "}
+    <a href="https://github.com/PortiESP/porti-web-cmd">Github repo</a>
+  </>,
+  `
 
-  > Type "help" to print the list of available commands
+  ┌─────────────┐
+  │ LETS START! │
+  └─────────────┘
+
+      > Type "help" to print the list of available commands
 
 
 `,
