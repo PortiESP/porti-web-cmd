2f057b6a18b9215371214fa84c90d7bf06a366c8
porti <portiesp@gmail.com>
2024-01-12 13:21:42 +0100

    Fixed styles

--- src/components/mips-decompiler/MipsDecompiler.jsx
+++ src/components/mips-decompiler/MipsDecompiler.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 import setup from "./scripts/setup.js"
-import scss from "./styles/index.module.scss"
+import scss from "./styles/mips_decompiler.module.scss"
 
 export default function MipsDecompiler() {
 
@@ -10,17 +10,17 @@ export default function MipsDecompiler() {
 
   return (
     <>
-      <main>
-        <div className="wrapper">
+      <main className={scss.main}>
+        <div className={scss.wrapper}>
           <h1>MIPS Instruction disassembler</h1>
           <div>
-            <div className="input-group">
+            <div className={scss.input_group}>
               <label htmlFor="instruction-address">Instruction Address ($PC): </label>
-              <input type="text" id="instruction-address" name="instruction-address" placeholder="0x1A2B3C4D" autoFocus /> <span className="required">*(required)</span>
+              <input type="text" id="instruction-address" name="instruction-address" placeholder="0x1A2B3C4D" autoFocus /> <span className={scss.required}>*(required)</span>
             </div>
-            <div className="input-group">
+            <div className={scss.input_group}>
               <label htmlFor="instruction-code">Instruction code (big-endian): </label>
-              <input type="text" id="instruction-code" name="instruction-code" placeholder="XX-XX-XX-XX" /> <span className="required">*(required)</span>
+              <input type="text" id="instruction-code" name="instruction-code" placeholder="XX-XX-XX-XX" /> <span className={scss.required}>*(required)</span>
             </div>
           </div>
           <hr />
@@ -30,8 +30,9 @@ export default function MipsDecompiler() {
           <div id="error-msg"></div>
         </div>
       </main>
-      <footer>
-        <span className="contribute">
+
+      <footer className={scss.footer}>
+        <span className={scss.contribute}>
           Contribute to this project in{" "}
           <a target="_blank" href="https://github.com/PortiESP/mips-disassembler-online">
             Github
